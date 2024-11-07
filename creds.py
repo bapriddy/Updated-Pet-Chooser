21d1bddb9ca8e9bb0411e5df80fec65d7d73da83
Cortes <bapriddy@users.noreply.github.com>
2024-11-07 17:26:47 -0600

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,4 @@
+username = 'cbcradio_bds6'
+password = 'Pineapple29Monkey^'
+hostname = 'cbcradio.org'
+database = 'cbcradio_bds754_6'
---
+++ pets.py
@@ -0,0 +1,8 @@
+class Pets:
+    def __init__(self, name, animal_type, owner, age):
+        self.name = name
+        self.animal_type = animal_type
+        self.owner = owner
+        self.age = age
+
+
